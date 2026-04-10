2037ab02a08e137e5270162f617f27998bced44d
Alparslan BIYIK <biyik78@hotmail.com>
2026-04-10 19:22:31 +0300

    Initial Commit

---
+++ Story App/Story_AppApp.swift
@@ -0,0 +1,32 @@
+//
+//  Story_AppApp.swift
+//  Story App
+//
+//  Created by Alparslan BIYIK on 10.04.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Story_AppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
